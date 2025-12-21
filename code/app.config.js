024c91e68fe93697a8489254bbf7d3baca045f55
AngusTan <tvhang7@gmail.com>
2025-12-21 12:12:50 +0800

    Updated logo

--- code/app.config.js
+++ code/app.config.js
@@ -31,6 +31,14 @@ export default {
     version: '1.0.0',
     plugins: ['expo-router'],
     scheme: 'm43x2mobile',
+    ios: {
+      "bundleIdentifier": "com.atpical.m43x2mobile",
+      "icon": "./assets/images/icon.png"
+    },
+    android: {
+      "package": "com.atpical.m43x2mobile",
+      "icon": "./assets/images/icon.png"
+    },
     extra: {
       ...getApiConfig(),
       environment: IS_DEV ? 'development' : IS_STAGING ? 'staging' : 'production',
